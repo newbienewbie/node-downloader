0840515f4532e76aa2d257bcfc38af29aa3214c9
newbienewbie <webbot@sina.com>
2017-06-11 23:12:51 +0800

    重构：改为Downloader类

--- test/downloader.test.js
+++ test/downloader.test.js
@@ -1,6 +1,6 @@
 const path=require('path');
 const assert=require('assert');
-const {download}=require('../lib/downloader');
+const Downloader=require('../lib/downloader');
 
 describe('downloader tester',function(done){
 
@@ -16,11 +16,13 @@ describe('downloader tester',function(done){
             console.log(`percentage: ${percentage} %`);
         };
         let url="https://www.baidu.com/img/bd_logo1.png";
-        return download(url,tempPath,callback);
+        const downloader=new Downloader();
+        const saveAs=path.join(tempPath,"fuck.png");
+        return downloader.download(url,callback,saveAs);
     });
 
     it('#download() large file timeout ',function(){
-        this.timeout(50000);
+        this.timeout(900000);
         let accumulated=0;
         const callback=(chunck,total)=>{
             accumulated+=Buffer.from(chunck).length;
@@ -28,6 +30,41 @@ describe('downloader tester',function(done){
             console.log(`percentage: ${percentage} %`);
         };
         let url="https://ftp.nluug.nl/pub/vim/pc/gvim80-586.exe";
-        return download(url,tempPath,callback,{timeout:1*1000});
+        const downloader=new Downloader({
+            baseDir:tempPath,
+            fetchOpts:{
+                timeout:0.01*60*1000
+            }
+        });
+        return downloader.download(url,callback);
     });
+
+    it('#downloadGroup() large file timeout ',function(){
+        this.timeout(900000);
+        let accumulated=0;
+        const callback=(chunck,total)=>{
+            accumulated+=Buffer.from(chunck).length;
+            const percentage=Number(accumulated/total*100).toFixed(2);
+            console.log(`percentage: ${percentage} %`);
+        };
+        const downloader=new Downloader({
+            baseDir:tempPath,
+            fetchOpts:{
+                timeout:60*60*1000
+            }
+        });
+        const taskArray=[
+            {url:'http://file.allitebooks.com/20170611/MCSA%2070-741%20Cert%20Guide.pdf',status:""},
+            {url:'http://file.allitebooks.com/20170611/Winning%20Design!,%202nd%20Edition.pdf',status:""}
+        ];
+        return downloader.downloadGroup(
+            taskArray,
+            t=>{
+                console.log(t);
+            }
+        );
+    });
+
+    
+
 });
